08afb3d7c595c6015bd72dc37320638cb489a65e
German-Zero <germannaz_@hotmail.com>
2026-03-11 15:47:28 -0300

    add api proxy

--- src/services/api/axios.instance.ts
+++ src/services/api/axios.instance.ts
@@ -1,18 +1,17 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
     withCredentials: true,
 })
 
-api.interceptors.request.use(config => {
-    if (typeof window !== 'undefined') {
-        const token = localStorage.getItem('token');
-        if (token) config.headers.Authorization = `Bearer ${token}`;
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
     }
-    return config;
-});
-
+  return config
+})
